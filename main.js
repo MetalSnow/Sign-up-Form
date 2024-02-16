43dae0c5acca3dd4a7b9f11b8402e8e07b9f4649
MetalSnow <yasuotrend@gmail.com>
2024-02-16 17:48:17 +0800

    Update form validation logic and styles.
    Removed the 'error' class from the password input container in the HTML file.
    Deleted the CSS rule targeting invalid password inputs in the CSS file.
    Adjusted JS validation logic to remove the condition checking if the password value is empty in the JavaScript file.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let errorClass = document.querySelector("#pwdDiv")
 
 
 function pwdMatch(){
-    if((pwd.value !== pwdConfirm.value) || pwd.value === "" ){
+    if(pwd.value !== pwdConfirm.value){
         errorClass.classList.add("error");
         pwd.style.borderColor = "red"
         pwdConfirm.style.borderColor = "red"
